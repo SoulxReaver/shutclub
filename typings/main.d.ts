64c525975ee0df7f3fbad8b1678d1ff43260d1e1
soulxreaver <alextong0628@gmail.com>
2016-05-10 21:23:00 -0700

    add facebook sdk

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,5 +1,5 @@
+/// <reference path="custom/inert.d.ts" />
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
 /// <reference path="main/ambient/hapi/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
 /// <reference path="main/ambient/systemjs/index.d.ts" />
-/// <reference path="custom/inert.d.ts" />
